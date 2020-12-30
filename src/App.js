c4191447058c11b13e283bbfbc96afe5cc10292b
Abinash <sabhinash88@gmail.com>
2020-12-30 23:23:24 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Countdown from './Countdown.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const currentDate = new Date();
+    const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
+    return (
+      <div className="center">
+        <h1><span>2021</span><br />New Year Countdown</h1>
+        <Countdown date={`${year}-01-01T00:00:00`} />
+      </div>
+    );
+  }
 }
 
 export default App;
